12b4cc869df0118c46483040b09a806db62b0cf0
Oleksandr Kovtun <oleksandr_kovtun3@epam.com>
2023-04-05 10:46:10 +0300

    add all dotnet labs

---
+++ lab3_3/lab3_3/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using lab3_3.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace lab3_3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int Id)
+        {
+            ViewBag.BookId = Id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = System.DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Thank you, " + purchase.Person + ", for buying!";
+        }
+    }
+}
---
+++ lab3_3/lab3_3/Models/BookDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab3_3.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Name 1", Author = "Author 1", Price = 250 });
+            db.Books.Add(new Book { Name = "Name 2", Author = "Author 2", Price = 200 });
+            db.Books.Add(new Book { Name = "Name 3", Author = "Author 3", Price = 150 });
+
+            base.Seed(db);
+        }
+    }
+}
